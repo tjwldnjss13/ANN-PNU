a7ee2726da66590cc493d46715bb370b4df2796d
SJW <tjwldnjss13@gmail.com>
2020-05-26 18:23:59 +0900

    locally connected

--- locally_connected.py
+++ locally_connected.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import os
+import matplotlib.pyplot as plt
 
 def preprocessed_image(image_file):
     image = np.array(image_file)
@@ -87,12 +88,37 @@ def softmax(x):
     return exps / np.sum(exps)
 
 
+def feedforward(image):
+    pre_H1 = np.zeros((8, 8))
+    for i in range(8):
+        for j in range(8):
+            pre_H1[i, j] = np.sum(image[2 * i:2 * i + 3, 2 * j:2 * j + 3] * W_ItoH1)
+    post_H1 = relu(pre_H1) + b_H1
+
+    pre_H2 = np.zeros((4, 4))
+    for i in range(4):
+        for j in range(4):
+            pre_H2[i, j] = np.sum(post_H1[i:i + 5, j:j + 5] * W_H1toH2)
+    post_H2 = relu(pre_H2) + b_H2
+
+    post_H2_flat = np.reshape(post_H2, 16)
+    pre_O = np.zeros(10)
+    for i in range(10):
+        pre_O[i] = np.sum(post_H2_flat * W_H2toO[:, i])
+    post_O = sigmoid(pre_O) + b_O
+
+    softmax_class = softmax(post_O)
+
+    return post_O, softmax_class
+
+
+
 SOBEL_X = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
 SOBEL_Y = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
 
 # Preprocessing image data
-train_images = []
-train_labels = []
+images = []
+labels = []
 data_path = './digit data'
 
 for n in range(10):
@@ -100,26 +126,30 @@ for n in range(10):
         fn = str(label) + '.' + str(n) + '.png'
         image_path = os.path.join(data_path, fn)
         image = Image.open(image_path).convert('L')
-        train_image = preprocessed_image(image)
+        image = preprocessed_image(image)
 
-        train_images.append(train_image)
-        train_labels.append(label)
+        images.append(image)
+        labels.append(label)
 
-train_images = np.array(train_images)
-train_labels = np.array(train_labels)
+images = np.array(images)
+labels = np.array(labels)
 
 labels_one_hot = []
-for i in range(len(train_labels)):
+for i in range(len(labels)):
     arr = np.zeros(10)
-    arr[train_labels[i]] = 1
+    arr[labels[i]] = 1
     labels_one_hot.append(arr)
 
-train_labels = np.array(labels_one_hot)
+labels = np.array(labels_one_hot)
+
+train_images, train_labels = images[:70], labels[:70]
+valid_images, valid_labels = images[70:85], labels[70:85]
+test_images, test_labels = images[85:], labels[85:]
 
 # Define parameters
-LR = .05
+LR = .1
 LR_b = .1
-epochs = 5000
+epochs = 4000
 
 W_ItoH1 = 2 * np.random.random((3, 3)) - 1
 W_H1toH2 = 2 * np.random.random((5, 5)) - 1
@@ -129,12 +159,20 @@ b_H1 = 2 * np.random.random((8, 8)) - 1
 b_H2 = 2 * np.random.random((4, 4)) - 1
 b_O = 2 * np.random.random(10) - 1
 
-# Train
+epoch_list = []
+train_loss_list = []
+valid_loss_list = []
+
+train_acc_list = []
+valid_acc_list = []
+
 for epoch in range(epochs):
+    epoch_list.append(epoch)
     print('[{:3d}/{} epoch]'.format(epoch+1, epochs), end='    ')
-    epoch_acc = 0
-    epoch_loss = 0
-    for data in range(100):
+    train_acc = 0
+    train_loss = 0
+    # Train
+    for data in range(len(train_images)):
         image = train_images[data]
         label = train_labels[data]
 
@@ -158,11 +196,14 @@ for epoch in range(epochs):
         post_O = sigmoid(pre_O) + b_O
 
         softmax_class = softmax(post_O)
+
+        # out, softmax_class = feedforward(image, label)
+
         softmax_ce = softmax_cross_entropy(label, softmax_class)
         if np.argmax(softmax_class) == np.argmax(label):
-            epoch_acc += 1
+            train_acc += 1
 
-        epoch_loss += softmax_ce
+        train_loss += softmax_ce
 
         # Backpropagate
         D_post_O = softmax_class - label
@@ -213,32 +254,65 @@ for epoch in range(epochs):
             for j in range(3):
                 W_ItoH1[i, j] -= LR * np.sum(image[i:i+15:2, j:j+15:2] * D_pre_H1)
 
-    epoch_loss /= 100
-    print('Accuracy : {}%, Loss: {}'.format(epoch_acc, epoch_loss))
+    train_acc /= len(train_images)
+    train_acc *= 100
+    train_loss /= len(train_images)
+    print('(Train) Accuracy : {:.1f}%, Loss : {:.5f}'.format(train_acc, train_loss), end='   ')
+    train_loss_list.append(train_loss)
+    train_acc_list.append(train_acc)
+
+    # Validation
+    valid_acc = 0
+    valid_loss = 0
+    for valid_idx in range(len(valid_images)):
+        image = valid_images[valid_idx]
+        label = valid_labels[valid_idx]
+
+        _, softmax_class = feedforward(image)
+        if np.argmax(softmax_class) == np.argmax(label):
+            valid_acc += 1
+        valid_loss += softmax_cross_entropy(label, softmax_class)
+    valid_acc /= len(valid_images)
+    valid_acc *= 100
+    valid_loss /= len(valid_images)
+    print('(Valid) Accuracy : {:.1f}%, Loss : {:.5f}'.format(valid_acc, valid_loss))
+    valid_loss_list.append(valid_loss)
+    valid_acc_list.append(valid_acc)
 
 
 # Test
-while True:
-    print('\nType file name : ')
-    fn = input()
-    test_image_path = os.path.join(data_path, fn)
-    test_image = preprocessed_image(Image.open(test_image_path).convert('L'))
+test_acc = 0
+test_loss = 0
+for test_idx in range(len(test_images)):
+    image, label = test_images[test_idx], test_labels[test_idx]
 
-    pre_H1 = np.zeros((8, 8))
-    for i in range(8):
-        for j in range(8):
-            pre_H1[i, j] = np.sum(test_image[2*i:2*i+3, 2*j:2*j+3] * W_ItoH1)
-    post_H1 = leaky_relu(pre_H1)
+    _, softmax_class = feedforward(image)
+    if np.argmax(softmax_class) == np.argmax(label):
+        test_acc += 1
+    test_loss += softmax_cross_entropy(label, softmax_class)
 
-    pre_H2 = np.zeros((4, 4))
-    for i in range(4):
-        for j in range(4):
-            pre_H2[i, j] = np.sum(post_H1[i:i+5, j:j+5] * W_H1toH2)
-    post_H2 = leaky_relu(pre_H2)
+test_acc /= len(test_images)
+test_acc *= 100
+test_loss /= len(test_images)
+print('                    (Test) Accuracy : {:.1f}, Loss : {:.5f}'.format(test_acc, test_loss))
 
-    post_H2_flat = np.reshape(post_H2, 16)
-    pre_O = np.zeros(10)
-    post_O = pre_O
-    for i in range(10):
-        pre_O[i] = np.sum(post_H2_flat * W_H2toO[:, i])
-    post_O = sigmoid(pre_O)
+epochs = np.array(epoch_list)
+train_losses = np.array(train_loss_list)
+valid_losses = np.array(valid_loss_list)
+
+train_acces = np.array(train_acc_list)
+valid_acces = np.array(valid_acc_list)
+
+plt.figure(0)
+plt.plot(epochs, train_losses, 'r-', label='Train loss')
+plt.plot(epochs, valid_losses, 'b:', label='Valid loss')
+plt.title('Train/Validation Loss')
+plt.legend()
+
+plt.figure(1)
+plt.plot(epochs, train_acces, 'r-', label='Train acc')
+plt.plot(epochs, valid_acces, 'b:', label='Valid acc')
+plt.title('Train/Validation Acc')
+plt.legend()
+
+plt.show()
